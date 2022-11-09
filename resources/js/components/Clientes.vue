01c3e010a74bed2a87049c2b9891468a6156a91a
jltf2308 <jtf2308@gmail.com>
2022-11-09 09:19:48 -0500

    first commit

---
+++ resources/js/components/Clientes.vue
@@ -0,0 +1,27 @@
+<template>
+    <div class="container">
+        <div class="row">
+            <div class="col-12">
+                <div class="card shadow-sm">
+                    <div class="card-header">
+                        <h3>Clientes</h3>
+                    </div>
+                    <div class="card-body">
+                        <p class="mb-0">Clientes</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name:"Clientes",
+    data(){
+        return {
+            // user:this.$store.state.auth.user
+        }
+    }
+}
+</script>
