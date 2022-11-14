0d717b2cbf19803e85ee8ff1a4c783f9024deeb5
jltf2308 <jtf2308@gmail.com>
2022-11-14 05:15:35 -0500

    fix

--- resources/js/components/common/tableComponent.vue
+++ resources/js/components/common/tableComponent.vue
@@ -6,7 +6,12 @@
                     <thead>
                         <tr>
                             <th class="text-center" v-for="(value, name) in header" :key="name">{{value}}</th>
-                            <th class="text-center w-25" v-if="buttons.length > 0">Acciones</th>
+                            <template v-if="typeof buttons != 'undefined'">
+                                <th class="text-center w-25">Acciones</th>
+                            </template>
+                            <template v-if="typeof actionsButtons != 'undefined'">
+                                <th class="text-center w-25">Acciones</th>
+                            </template>
                         </tr>
                     </thead>
                     <tbody>
@@ -21,36 +26,23 @@
                                 <template v-for="(val, index) in value">
                                     <td class="align-middle" v-if="index != 'id' && index != 'type'">{{val}}</td>
                                 </template>
-                                <td class="text-center align-middle p-1" v-if="buttons.length > 0">
-                                    <div class="btn-group" role="group" aria-label="Basic example">
-                                        <template v-for="(nameButton, name) in buttons" :key="name">
-                                            <routerComponent
-                                                :routeParams="nameButton.routeParams"
-                                                :classStyle="nameButton.classStyle"
-                                                :title="nameButton.title"
-                                                :icon="nameButton.icon"
-                                                :paramId="value.id"
-                                                :paramName="value.business_name"
-                                            ></routerComponent>
-                                            <!-- <router-link 
-                                                :to="{
-                                                    name:nameButton.to, 
-                                                    params:{ 
-                                                        companyId:value.id,
-                                                        companyName:value.bussinesName
-                                                    },
-                                                }" 
-                                                :class="nameButton.color"
-                                                :title="nameButton.toolTip"
-                                                class="btn btn-sm" data-bs-toggle="tooltip"  data-bs-placement="bottom">
-                                                <i class="material-icons text-white">{{nameButton.icon}}</i>
-                                            </router-link> -->
-                                            <!-- <button type="button" class="btn btn-sm" :class="value.color" data-bs-toggle="tooltip" data-bs-placement="bottom" :title="value.toolTip">
-                                                <i class="material-icons text-white">{{value.icon}}</i>
-                                            </button> -->
-                                        </template>
-                                    </div>
-                                </td>
+                                <template v-if="typeof buttons != 'undefined'">
+                                    <td class="text-center align-middle p-1" v-if="buttons.length > 0">
+                                        <div class="btn-group" role="group" aria-label="Basic example">
+                                            <template v-for="(nameButton, name) in buttons" :key="name">
+                                                <routerComponent
+                                                    :routeParams="nameButton.routeParams"
+                                                    :classStyle="nameButton.classStyle"
+                                                    :title="nameButton.title"
+                                                    :icon="nameButton.icon"
+                                                    :paramId="value.id"
+                                                    :paramName="value.business_name"
+                                                ></routerComponent>
+                                                
+                                            </template>
+                                        </div>
+                                    </td>
+                                </template>
                             </tr>  
                         </template>                      
                     </tbody>
@@ -61,7 +53,7 @@
 </template>
 
 <script>
-    import routerComponent from './routerComponent.vue'
+    import routerComponent from './routerComponent.vue';
     export default {
         name:"tableComponent",
         components:{
@@ -78,8 +70,11 @@
             },
             buttons:{
                 type:Array,
-                required:true,
+            },
+            actionsButtons:{
+                type:Array,
             }
+
         },
         data(){
             return {
@@ -90,7 +85,7 @@
 
         },
         mounted(){
-            // console.log(Object.keys(this.header).length);
+            console.log(this.buttons);
         }
     }
 </script>
--- resources/js/components/forms/AsociarProfesionalesForm.vue
+++ resources/js/components/forms/AsociarProfesionalesForm.vue
@@ -21,47 +21,70 @@
                 <div class="caja sm mb-15 especial">
                     <h2 class="inline-b mr-15">Selecciona el/los profesional(es) que deseas asociar al cliente </h2>
                     <!-- first_name -->
-                    <div class="form-group" :class="{'error':invalid.first_name}">
-                        <label for="first_name">Nombre*:</label>
-                        <input type="text" name="first_name" class="form-control" id="first_name" placeholder="Nombre" v-model="personData.first_name">
-                        <p class="message" v-for="(e_first_name, index) in errors.first_name" :key="index">{{e_first_name}}</p>
+                    <div class="form-group" :class="{'error':invalid.professional}">
+                        <label for="professional">Profesional: *</label>
+                        <select  name="professional" id="professional" class="form-control" v-model="professionalSelected.id" @change="onChange($event.target.value)">
+                            <option disabled value="">Selecciona</option>
+                            <option 
+                            v-for="selectedProfessional in professionals" 
+                            :key="selectedProfessional.id" 
+                            :value="selectedProfessional.id">
+                                {{ selectedProfessional.fullName}}
+                            </option>
+                        </select>
+                        <p class="message" v-for="(e_professional, index) in errors.professional" :key="index">{{e_professional}}</p>
+                        <!-- <p class="message">Selecciona el país.</p> -->
+                    </div>  
+                    <p class="small black">* Datos obligatorios </p>
+                    <div class="row">
+                        <div class="form-group col-6">
+                            <label for="dateInit">Fecha de inicio:</label>
+                            <Datepicker 
+                                :enableTimePicker="false"
+                                :closeOnAutoApply="true"
+                                position="right"
+                                locale="es"
+                                :format-locale="es"
+                                inline autoApply
+                                v-model="professionalSelected.dateInit"
+                            ></Datepicker>
+                        </div>
+                        <div class="form-group col-6">
+                            <label for="dateEnd">Fecha final:</label>
+                            <Datepicker 
+                                :enableTimePicker="false"
+                                :closeOnAutoApply="true"
+                                position="right"
+                                :format-locale="es"
+                                inline autoApply
+                                v-model="professionalSelected.dateEnd"
+                            ></Datepicker>
+                        </div>
                     </div>
-                    <!-- last_name -->
-                    <div class="form-group" :class="{'error':invalid.last_name}">
-                        <label for="last_name">Apellido*:</label>
-                        <input type="text" name="last_name" class="form-control" id="last_name" placeholder="Apellido" v-model="personData.last_name">
-                        <p class="message" v-for="(e_last_name, index) in errors.last_name" :key="index">{{e_last_name}}</p>
+                    <br>
+                    <div class="col-12 col-md-8 col-lg-6 text-end hidden-md-down">
+                        <button type="button" class="button main" @click.prevent="addProfessional()">Agregar Profesional</button>
                     </div>
-                    <!-- register_number -->
-                    <div class="form-group" :class="{'error':invalid.register_number}">
-                        <label for="register_number">Número de identificación*:</label>
-                        <input type="text" name="register_number" class="form-control" id="register_number" placeholder="Número de identificación" v-model="personData.register_number">
-                        <p class="message" v-for="(e_register_number, index) in errors.register_number" :key="index">{{e_register_number}}</p>
+                    <br>
+                    <div class="row">
+                        <div class="col-6">
+                            <h3>Profesionales Seleccionados</h3>
+                        </div>
+                        <TableComponent :header="tableData.header" :body="tableData.data"></TableComponent>
                     </div>
-                    <!-- email -->
-                    <div class="form-group" :class="{'error':invalid.email}">
-                        <label for="email">Correo Electrónico*:</label>
-                        <input type="text" name="email" class="form-control" id="email" placeholder="Correo Electrónico" v-model="personData.email">
-                        <p class="message" v-for="(e_email, index) in errors.email" :key="index">{{e_email}}</p>
-                    </div>
-                    <!-- position -->
-                    <div class="form-group" :class="{'error':invalid.position}">
-                        <label for="position">Cargo*:</label>
-                        <input type="text" name="position" class="form-control" id="position" placeholder="Cargo" v-model="personData.position">
-                        <p class="message" v-for="(e_position, index) in errors.position" :key="index">{{e_position}}</p>
-                    </div>
-                    
                 </div>
+
+                
             </div>
             
             <div v-if="activeItem == 1" class="col-12 tab-pane fade" :class="{ 'active show': isActive(1) }" id="1">
                 <div class="caja mb-30">
-                    <h3 class="inline-b mr-15">Datos del profesional</h3>
-                    <p><span class="bold">Nombre: </span><br> {{ personData.first_name }} </p>
+                    <h3 class="inline-b mr-15">Profesionales Seleccionados</h3>
+                    <!-- <p><span class="bold">Nombre: </span><br> {{ personData.first_name }} </p>
                     <p><span class="bold">Apellido: </span><br> {{ personData.last_name }} </p>
                     <p><span class="bold">Número de identificación: </span><br> {{ personData.register_number }} </p>
                     <p><span class="bold">Correo Electrónico: </span><br> {{ personData.email }} </p>
-                    <p><span class="bold">Cargo: </span><br> {{ personData.position }} </p>
+                    <p><span class="bold">Cargo: </span><br> {{ personData.position }} </p> -->
                 </div>
             </div>
         </div>
@@ -85,7 +108,16 @@
 </template>
 <script>
     import axios from 'axios';
+    import Datepicker from '@vuepic/vue-datepicker';
+    import '@vuepic/vue-datepicker/dist/main.css'
+    import { es } from 'date-fns/locale'
+    import { ref } from 'vue'
+    import TableComponent from '../common/tableComponent.vue';
     export default {
+        components: {
+            Datepicker,
+            TableComponent
+        },
         props:{
             companyId: {
                 required: true,
@@ -93,21 +125,39 @@
         },
         data() {
             return {
+                es:es,
                 tabs:{
-                    0: 'Datos del profesional',
+                    0: 'Seleccione profesional',
                     1: 'Resumen',
                 },
                 activeItem: 0,
                 personData:{},
                 invalid:{
-                    first_name:0,
-                    last_name:0,
-                    register_number:0,
-                    email:0,
-                    position:0,
+                    professional:0,
+                    initDate:0,
+                    endDate:0,
                 },                
                 errors:{},
-                processing:false
+                processing:false,
+                professionalSelected:{
+                    id:'',
+                    dateInit:ref(),
+                    dateEnd:ref()
+                },
+                professionals:[],
+                tableData:{
+                    header:{
+                        0:'#',
+                        1:'Profesional',
+                        2:'Fecha inicio',
+                        3:'Fecha Fin'
+                    },
+                    data:[],
+                    buttons:{
+
+                    }
+                }
+                
             }
         },
         methods: {
@@ -221,7 +271,29 @@
                 }).finally(()=>{
                     this.processing = false
                 })
+            },
+            async getPeople(){
+                this.processing = true;
+                await axios.get('/sanctum/csrf-cookie');
+                await axios.get('/api/person').then(({data})=>{
+                    this.professionals = data.people;
+                }).catch(({response})=>{
+                    if(response.status===422){
+                        this.validationErrors = response.data.errors
+                    }else{
+                        this.validationErrors = {}
+                        alert(response.data.message)
+                    }
+                }).finally(()=>{
+                    this.processing = false
+                })
+            },
+            addProfessional(){
+                console.log(this.professionalSelected)
             }
         },
+        mounted(){
+            this.getPeople();
+        }
     }
 </script>
